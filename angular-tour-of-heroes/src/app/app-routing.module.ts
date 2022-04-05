388a81e5f141c212ddfd3f4ebfb9f99c8f0ae37e
Dalton Hodkiewicz <dhodkiewicz@gmail.com>
2022-04-05 16:43:57 -0500

    update
    4/5/22 4:43PM

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,12 +1,35 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
+//configures the routes (views to display when a user clicks a link or pastes a URL into the browser address bar)
+//Typical angular Route has two properties:
+// -- the path : string that matches the URL in the browser address bar 
+// -- component : the component that the router should create when navigating to the route
+// this tells the router to match that URL to path: 'heroes' and display the HeroesComponent when the URL is something like
+// localhost:4200/heroes
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent }, // redirects on initial load
+  { path: 'heroes', component: HeroesComponent }
+];
 
+/*
+-- The @NgModule metadata initializes the router and starts it listening for browser location changes.
+-- The following line adds the RouterModule to the AppRoutingModule 
+imports array and configures it with the routes in one step by calling RouterModule.forRoot():
+*/
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+/*
+  The method is called forRoot() because you configure the router at the application's root level. 
+  The forRoot() method supplies the service providers and directives needed for routing,
+  and performs the initial navigation based on the current browser URL.
+*/
+export class AppRoutingModule { } // exports RouterModule so that way it's available throughout the application
--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,26 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; //ngModel lives within this import
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component'; // the main application component
-import { HeroesComponent } from './heroes/heroes.component'; // the Hero component
-import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // the Hero detail component
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './app-routing.module'; // implement messages component
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    MessagesComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
